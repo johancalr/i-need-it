be09a5a55129059fe482e1a4c3e6185978bab497
Johan Camilo Lozano Ramírez <johancalr11@gmail.com>
2024-03-11 13:24:50 -0500

    Consume la API de platzi para listar productos

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
+import { Card } from "../../Components/Card";
 import { PagesLayout } from "../../Layouts/PagesLayout";
+import { useEffect } from "react";
 
 function Home() {
-
+  const [products, setProducts] = useState(null);
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+    .then(respose => respose.json())
+    .then(data => setProducts(data));
+  }, [])
   return (
     <PagesLayout>
       Home
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {
+          products?.map(product => (
+            <Card
+              key={product.id}
+              data={product}
+            />
+          ))
+        }
+      </div>
     </PagesLayout>
   )
 }
